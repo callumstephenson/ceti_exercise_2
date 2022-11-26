9fa242b4e4b42d9bb7089a0d33c26fcbd48d6a35
callumstephenson <callumstephenson03@gmail.com>
2022-11-26 14:07:11 +0000

    fix

--- runscript_task2.m
+++ runscript_task2.m
@@ -11,7 +11,7 @@ l_c = 130;
 l_d = 0;
 
 % roughness
-roughness = 0.0046e-3;
+roughness = 4.5e-5;
 
 % heights
 h_0 = 1.3;
@@ -33,6 +33,7 @@ rho = 998;
 
 vels_u3 = linspace(1, 2.8, 10);
 for i = 1:length(vels_u3)
+    cf_3(i) = cf_finder(vels_u3(i), roughness, d_a);
     vels_pure(i) = split_vel_finder_task2(vels_u3(i), g, roughness, rho);
     vels_u4(i) = double(vels_pure(i).u_4(1,1));
     vels_u5(i) = double(vels_pure(i).u_5(1,1));
@@ -40,7 +41,7 @@ for i = 1:length(vels_u3)
     cf_5(i) = cf_finder(vels_u5(i), roughness, d_b);
     p1_vals(i) = (1.3 * rho * g) - (vels_u3(i).^2 * rho);
     p3_vals(i) = rho * (vels_u4(i).^2 - (0.5 * vels_u3(i).^2) + (2 * cf_4(i) * vels_u4(i).^2 * l_c / d_c));
-    p2_vals(i) = p3_vals(i) + (vels_u3(i).^2 * rho * 0.5) * (1 + (4 * cf_finder(vels_u3(i), roughness, d_a) * l_a / d_a)) + (6 * rho * g);
+    p2_vals(i) = p3_vals(i) + (vels_u3(i).^2 * rho * 0.5) * (1 + (4 * cf_3(i) * l_a / d_a)) + (6 * rho * g);
     p2_minus_p1(i) = p2_vals(i) - p1_vals(i);
     vol_flowrate(i) = (vels_u3(i) * d_a.^2 * 0.25 * pi);
     P_pumps(i) = 2 * (a - (b * vol_flowrate(i).^2));
